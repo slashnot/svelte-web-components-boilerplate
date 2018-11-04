c79adc2d989029e6984bb908ed05bc0360b166da
Ramkumar K <slashnot84@gmail.com>
2018-11-04 18:01:00 +0530

    Render using @html custom tag

--- src/services/TemplateEngine.js
+++ src/services/TemplateEngine.js
@@ -1,18 +1,40 @@
 let TemplateEngine = function (tpl, data) {
-    let re = /%-([^-%']+)?-%/g, code = 'let r=[];\n', cursor = 0, match;
-    let add = function(line, js) {
-        js? code += 'r.push(' + line + ');\n' :
+    let re = /%-([^-%']+)?-%/g,
+        code = 'let r=[];\n',
+        cursor = 0,
+        match;
+    let add = function (line, js) {
+        js ? code += 'r.push(' + line + ');\n' :
             code += 'r.push("' + line.replace(/"/g, '\\"') + '");\n';
     }
 
-    while(match = re.exec(tpl)) {
+    while (match = re.exec(tpl)) {
         add(tpl.slice(cursor, match.index));
-        add(match[1], true);     cursor = match.index + match[0].length;
+        add(match[1], true);
+        cursor = match.index + match[0].length;
     }
-  
+
     add(tpl.substr(cursor, tpl.length - cursor));
     code += 'return r.join("");';
     return new Function(code.replace(/[\r\t\n]/g, '')).apply(data);
+}
+
+const renderTemplate = () => {
+    let tpl = this.innerHTML;
+    let tplHtml = TemplateEngine(tpl, {
+        name: "Ram",
+        profile: {
+            age: 30
+        }
+    });
+    this.innerHTML = tplHtml;
+}
+
+const htmlToElement = (html) => {
+    var template = document.createElement("template");
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.childNodes;
 }
 
 export default TemplateEngine;
