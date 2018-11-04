cc3fc46589e56b3b55755e807ccd7a5ae561fdad
Ramkumar K <slashnot84@gmail.com>
2018-11-04 19:28:11 +0530

    Dynamic grid columns

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import App from './App.html';
 import Resource from './components/sl-resource';
 import GridList from './components/sl-grid-list';
-import TextField from './components/sl-text-field';
+// import TextField from './components/sl-text-field';
